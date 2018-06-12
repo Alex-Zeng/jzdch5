d481dbfae5cdcf1b6884dc5b4eea76492724a760
杨旭 <yangxu@jizhongdiancai.com>
2018-06-12 11:57:28 +0800

    id

--- src/components/login/login-code.vue
+++ src/components/login/login-code.vue
@@ -70,6 +70,7 @@ export default {
       axios.get('api/captcha/img', this.mobile).then((response) => {
         console.log(response)
         if (response.data.status === 0) {
+          this.id = response.data.data.id
           this.imgCodeSrc = response.data.data.src + '?t=' + new Date().getTime()
         } else {
           this.$vux.toast.show({
