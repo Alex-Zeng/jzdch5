1f4308cfb919909ce92c334d64ae4fdf89c4a6bb
杨旭 <yangxu@jizhongdiancai.com>
2018-07-11 16:45:06 +0800

    bug修改

--- src/components/login/login-code.vue
+++ src/components/login/login-code.vue
@@ -306,7 +306,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   body {
     background: #FFFFFF;
   }
--- src/components/login/register.vue
+++ src/components/login/register.vue
@@ -303,7 +303,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   body {
     background: #FFFFFF;
   }
